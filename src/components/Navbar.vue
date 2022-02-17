f9c5693d30615743ad2a7c282dd1273770ddaf83
Alex Archer <alex.archer@okta.com>
2022-02-17 13:12:14 -0500

    style: add padding to HelloWorld component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top custom-bg-dark">
       <a class="navbar-brand" href="#">
-        <img style="max-height:25px" src="../assets/logo.png" />Task Manager
+        <img style="max-height:25px" src="../assets/logo.png" /> Task Manager
         </a>
       <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       </button> -->
