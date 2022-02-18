278ec9182702e4d224a42726aed659bcc64d341a
Alex Archer <alex.archer@okta.com>
2022-02-18 11:34:58 -0500

    style: fix link highlighting and add exact to links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,23 +10,23 @@
       </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <ul class="navbar-nav ml-auto mb-2 mb-md-0">
-        <li class="nav-item active">
-          <router-link to="/" class="nav-link">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link" exact>
             Home
           </router-link>
         </li>
         <li class="nav-item">
-          <router-link to="/tasks" class="nav-link">
+          <router-link to="/tasks" class="nav-link" exact>
             Tasks
           </router-link>
         </li>
         <li class="nav-item">
-          <router-link to="/register" class="nav-link">
+          <router-link to="/register" class="nav-link" exact>
             Register
           </router-link>
         </li>
         <li class="nav-item">
-          <router-link to="/login" class="nav-link">
+          <router-link to="/login" class="nav-link" exact>
             Login
           </router-link>
         </li>
--- src/router/index.js
+++ src/router/index.js
@@ -48,6 +48,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
+  linkActiveClass: 'active',
   mode: 'history',
   base: process.env.BASE_URL,
   routes
